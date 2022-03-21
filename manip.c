4fe408a55322a8094dce12866836ab60a47604b8
robertwarnold <95994272+robertwarnold@users.noreply.github.com>
2022-03-20 21:16:26 -0400

    Update manip.c

--- manip.c
+++ manip.c
@@ -87,60 +87,60 @@ void mirror(filePtr_t *image, filePtr_t **theList, int count){
  }
 
 //The purpose of this function is to seperate the orange tiger paw logo pixel by pixel from the purple background.
-void green_screen(filePtr_t *image1, filePtr_t *image2, filePtr_t **theList, int count){
-
-   header_t header = image2->theImage->header;
-   image_t *resultImage = allocate_memory(header);
-   resultImage->header = header;
-
-   char fileName[30] = "gs";
-   strcat(fileName, image2->imageFileName);
-
-   if(fileName[2] != 's'){
-        char buf[5];
-        fileName[strlen(fileName)-4] = '\0';
-        int length = snprintf(buf, 5, "%d", count);
-        char *imageName = (char *)malloc(length);
-        sprintf(imageName, "%s%02d.ppm", fileName, count);
-
-        FILE *greenImage = fopen(imageName, "w");
-        if(!greenImage){
-          fprintf(stderr, "green screen image file could not open\n\n");
-
-          exit(1);
-
-  }
-
-   for(int row = 0; row < header.height; row++){
-     for(int col = 0; col < header.height; col++){
-       if(image1->theImage->pixels[row][col].r == 0){
-         if(image1->theImage->pixels[row][col].g == 255){
-           if(image1->theImage->pixels[row][col].b == 0){
-
-              resultImage->pixels[row][col].r = image2->theImage->pixels[row][col].r;
-
-              resultImage->pixels[row][col].g = image2->theImage->pixels[row][col].g;
-
-              resultImage->pixels[row][col].b = image2->theImage->pixels[row][col].b;
-            }
-          }
-        }
-
-      else{
-              resultImage->pixels[row][col].r = image1->theImage->pixels[row][col].r;
-
-              resultImage->pixels[row][col].g = image1->theImage->pixels[row][col].g;
-
-              resultImage->pixels[row][col].b = image1->theImage->pixels[row][col].b;
-      }
-    }
-  }
-      write_p6(greenImage, resultImage);
-      fclose(greenImage);
-      addToList(*theList, greenImage, resultImage, imageName);
-}
+void green_screen (filePtr_t* image1, filePtr_t* image2, filePtr_t** theList, int count) {
+	int row, col = 0;
 
 
+   header_t header = image1->theImage->header;
+
+	// Create new image
+	image_t* newImage = allocate_memory(header);
+
+	// Create new file and open for writing
+	char fileName[30] = "gs"; // for green screen
+	strcat(fileName, image2->imageFileName);
+
+	// see if image already exists in list, if g is second, then yes
+	if ((fileName[2] != 'g') && (fileName[3] != 's')) {
+		char buf[5];
+		fileName[strlen(fileName)-4] = '\0';
+		int length = snprintf(buf, 5, "%d", count);
+		// snprintf(buf, 5, "%d", count);
+		char* imageName = (char*)malloc(length + 1);
+		sprintf(imageName, "%s%02d.ppm", fileName, count);
+
+		FILE* gsImage = fopen(imageName, "w");
+		if (!gsImage) {
+			fprintf(stderr, "green screen image file could not open\n\n");
+			exit(1);
+		}
+
+		// copy header info over
+		newImage->header = header;
+
+		// green screen algorithm
+		for (row = 0; row < header.height; row++) {
+			for (col = 0; col < header.width; col++) {
+				// check image2's pixel value for green
+				if ((image1->theImage->pixels[row][col].g == 255) &&
+					((image1->theImage->pixels[row][col].b == 0) &&
+					(image1->theImage->pixels[row][col].r == 0))) {
+					newImage->pixels[row][col] = image2->theImage->pixels[row][col];
+				}
+
+				// otherwise
+				else {
+					newImage->pixels[row][col] = image1->theImage->pixels[row][col];
+				}
+			}
+		}
+		// call write_p6 with new image
+		write_p6(gsImage, newImage);
+		fclose(gsImage);
+
+		//add to list
+		addToList(*theList, gsImage, newImage, imageName);
+	}
 }
 
 
